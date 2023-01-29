1e20a19f9cebce8075f779c51c921392af7a85db
Ankit <ankit.kumar31121993@gmail.com>
2023-01-29 18:16:56 +0530

    Added comments

--- AsyncTutorial/ApiRequestTests/NetworkRequestTests.swift
+++ AsyncTutorial/ApiRequestTests/NetworkRequestTests.swift
@@ -40,22 +40,28 @@ final class NetworkRequestTests: XCTestCase {
     }
 }
 
+//MARK: - Stubbing classed
+
+// Network error scenarios
 enum NetworkError: Error {
     case invalidURL, dataNotFound, dataIsEmpty
 }
 
-
+// This class is for sending networks requests and getting response back
 class MockNetworkRequest: NetworkRequest {
+    // Requesting response for the searched strinng.
     func getSearchData(searchMessage search: String) async throws -> [FeedModel] {
         let response = try await getApiResponseData(from: "https://api.giphy.com/v1/gifs/search?message=\(search)")
         return response.data
     }
     
+    // Getting trending data response
     func getTrendingData() async throws -> [FeedModel] {
         let response = try await getApiResponseData(from: "https://api.giphy.com/v1/gifs/trending")
         return response.data
     }
     
+    // It helps to make actual network calls.
     func getApiResponseData(from url: String) async throws -> FeedResponseModel {
         guard let url = URL(string: url) else {
             throw NetworkError.invalidURL
